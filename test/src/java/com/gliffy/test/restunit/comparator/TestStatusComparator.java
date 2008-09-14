3c2b55ed3a5b2f12d3d038604f9e9f7315bffcb3
David Copeland <davetron5000@gmail.com>
2008-09-13 22:25:39 -0400

    better descriptions for status tests

--- test/src/java/com/gliffy/test/restunit/comparator/TestStatusComparator.java
+++ test/src/java/com/gliffy/test/restunit/comparator/TestStatusComparator.java
@@ -20,23 +20,23 @@ public class TestStatusComparator
         HttpResponse httpErrorResponse = new HttpResponse();
         httpErrorResponse.setStatusCode(404);
         return new Object[][] {
-            { httpResponse, TestFactory.getRandomResponse(200), true, },
-            { httpResponse, TestFactory.getRandomBodyResponse(200), true, },
-            { httpErrorResponse, TestFactory.getRandomResponse(404), true, },
-            { httpErrorResponse, TestFactory.getRandomBodyResponse(404), true, },
-            { httpErrorResponse, TestFactory.getRandomResponse(400), false, },
-            { httpErrorResponse, TestFactory.getRandomBodyResponse(400), false, },
-            { httpResponse, TestFactory.getRandomResponse(0), false, },
-            { httpResponse, TestFactory.getRandomBodyResponse(0), false, },
+            { httpResponse, TestFactory.getRandomResponse(200), true, "Ensure that tests with same status are equal" },
+            { httpResponse, TestFactory.getRandomBodyResponse(200), true, "Ensure that tests with same status are equal for body response " },
+            { httpErrorResponse, TestFactory.getRandomResponse(404), true, "Ensure that tests with the same non-OK HTTP status are equal" },
+            { httpErrorResponse, TestFactory.getRandomBodyResponse(404), true, "Ensure success for same non-OK HTTP status for body response" },
+            { httpErrorResponse, TestFactory.getRandomResponse(400), false, "Check that different status fails" },
+            { httpErrorResponse, TestFactory.getRandomBodyResponse(400), false, "Check that different status fails for body response" },
+            { httpResponse, TestFactory.getRandomResponse(0), false, "Check that invalid HTTP status fails" },
+            { httpResponse, TestFactory.getRandomBodyResponse(0), false, "Check that invalid HTTP status fails for a body response" },
         };
     }
         
     @Test(dataProvider = "statusTests")
-    public void test(HttpResponse httpResponse, RestTestResponse testResponse, boolean match)
+    public void test(HttpResponse httpResponse, RestTestResponse testResponse, boolean match, String description)
     {
         StatusComparator comparator = new StatusComparator();
         ComparisonResult result = comparator.compare(httpResponse,testResponse);
 
-        assert match == result.getMatches() : "Expected " + (match ? "comparison to match" : "comparison to not match") + (result.getMatches() ? "" : ("(" + result.getExplanation() + ")") );
+        assert match == result.getMatches() : description + "; Expected " + (match ? "comparison to match" : "comparison to not match") + (result.getMatches() ? "" : ("(" + result.getExplanation() + ")") );
     }
 }
