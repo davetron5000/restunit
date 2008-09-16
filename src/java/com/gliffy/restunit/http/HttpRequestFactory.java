908bb8eb10f7ad7babded301715a1ef535ea66e0
David Copeland <davetron5000@gmail.com>
2008-09-15 20:02:54 -0400

    Added license stuff for [#2 state:resolved]
    Put in a small header and a checkstyle check for it.

--- src/java/com/gliffy/restunit/http/HttpRequestFactory.java
+++ src/java/com/gliffy/restunit/http/HttpRequestFactory.java
@@ -1,3 +1,8 @@
+// Copyright (c) 2008 by David Copeland
+// Licensed under the Apache License, Version 2.0
+// Available in LICENSE.txt with this code or
+// at http://www.apache.org/licenses/LICENSE-2.0
+
 package com.gliffy.restunit.http;
 
 import java.io.*;
